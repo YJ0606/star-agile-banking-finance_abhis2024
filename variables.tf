fe79c9e9a1ea840f3fed33f079440771dab1c53d
abhis2024 <164902481+abhis2024@users.noreply.github.com>
2024-09-11 11:25:25 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,9 +5,9 @@ variable "instance_type" {
 
 variable "ssh_private_key" {
   type    = string
-#   default = "/var/lib/jenkins/.ssh/id_ed25519"
+
 }
 variable "ssh_public_key" {
   type    = string
-#   default = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOKLtaq+uZZJid2gCdP842HxKwv/hHqh67hsnVlLw917"
+
 }
